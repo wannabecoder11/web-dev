e7393ee4da5bf8a9cf29a4830d6bf6188ca769a4
WannaBeCoder <wannabecoder11@gmail.com>
2025-10-25 12:50:00 +0530

    shopping list done, li deletion works and focuns also works

--- shopping-list/index.js
+++ shopping-list/index.js
@@ -6,11 +6,26 @@ const button = document.querySelector("button");
 function submit(element) {
     element.preventDefault();
     const listItem = document.createElement('li');
+    const span = document.createElement('span');
+    const liButton = document.createElement('button');
+    listItem.appendChild(span);
+    listItem.appendChild(liButton);
 
-    listItem.innerText = `${input.value}`;
-
+    span.innerText = `${input.value}`;
+    liButton.textContent = "Delete";
+    liButton.id = "listButton"
     ul.appendChild(listItem);
     input.value = "";
+    liButton.addEventListener("click", function(e) {
+        const li = e.target.closest('li'); // Find the closest <li> ancestor
+        if (li) {
+            li.remove(); // Remove the entire list item from DOM
+        }
+        input.focus()
+});
+
 };
+
+
 
 button.addEventListener("click", submit);
